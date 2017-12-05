b66a54682c70f5826c74470ccdcf318ea75cf357
saneeshkunjunni <saneeshkunjunni@hotmail.com>
2017-12-05 09:19:36 +0530

    a

---
+++ src/FashionModeling.DAL/Entity/JobRoles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionModeling.DAL.Entity
+{
+    public class JobRoles:UserEntity
+    {
+        public string RoleName { get; set; }
+        public string Professions { get; set; }
+        public string Gender { get; set; }
+        public int AgeFrom { get; set; }
+        public int AgeTo { get; set; }
+        public string BasedIn { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ src/FashionModeling.DAL/Entity/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionModeling.DAL.Entity
+{
+    public class Subscription
+    {
+        public long Id { get; set; }
+        public string EmailAddress { get; set; }
+        public bool Status { get; set; }
+    }
+}
