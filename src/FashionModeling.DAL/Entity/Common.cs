bd71922f25279e08280bbf8dc73478f1b4a4c4dd
saneeshkunjunni <saneeshkunjunni@hotmail.com>
2017-12-08 10:21:17 +0530

    up

--- src/FashionModeling.DAL/Entity/Common.cs
+++ src/FashionModeling.DAL/Entity/Common.cs
@@ -6,12 +6,11 @@ using System.Threading.Tasks;
 
 namespace FashionModeling.DAL.Entity
 {
-    public class Common:UserEntity
+    public class Common : UserEntity
     {
         public string Title { get; set; }
         public string Description { get; set; }
         public string Code { get; set; }
-        public bool  IsActive { get; set; }
-        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
     }
 }
